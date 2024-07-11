0bf6666ddc21bf0a37b3f111a870bd16ae0eb7dc
Mohameedshawky <133530678+Mohameedshawky@users.noreply.github.com>
2024-07-11 19:57:23 +0300

    Update authControllers.js - Add bcrypt for password hashing
    Implemented bcrypt to hash user passwords before saving them to the database in the register endpoint for enhanced security. 
    Added logic in the login endpoint to compare hashed passwords for authentication.

--- node-task-auth/Controllers/authControllers.js
+++ node-task-auth/Controllers/authControllers.js
@@ -1,26 +1,48 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 
 
+
+
+//handling errors comes from our model restrictions
+const handleErrors = (err)=>{ 
+  let errors = {name:'',email:'',password:''};
+  if(err.message.includes('user validation failed')){  //for handling missing name or invalid email or password <6 chars
+    Object.values(err.errors).forEach(({properties})=>{
+      errors[properties.path]=properties.message;
+    });
+    return errors;
+  }
+  else if(err.code=11000){ // handlind email uniquness *** but it will be cannot used at all as in post request it handled before catch an error
+    errors.email='that email is already existed';
+    return errors;
+  }
+};
 
 login_GET = (req,res)=>{
   res.render('login'); 
 };
 
 login_POST = async(req,res)=>{
-  
-  const body = req.body;
-  const user = await User.findOne({email:body.email,password:body.password})
-    if(user){
+  const {email,password} = req.body;
+  try{
+  const user = await User.findOne({email:email})
+    if(!user){
+      res.render('loginerror',{ error: 'Invalid email or password' });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).render('loginerror',{ error: 'Invalid email or password' });
+    }
       const token = jwt.sign({name:user.name},'secretKey',{expiresIn:'2h'});
       res.cookie('jwt',token).redirect('/home');
-
       console.log('login succeded');
-        }
-    else{
-      res.render('loginerror');
-        }     
+       
+  }
+  catch(error){
+    res.json(error);
+  }
 };
 
 register_GET = (req,res)=>{
@@ -28,22 +50,21 @@ register_GET = (req,res)=>{
 };
 
 register_POST = async (req, res) => {
+    const body = req.body;
     try {
-      const body = req.body;
-
       const user = await User.findOne({ email: body.email });
-  
       if (user) {
         res.render('alreadyRegistered');
       } else {
-        const newUser = new User(body);
-        const result = await newUser.save();
-        console.log('Saved user:', result);
+        const salt = await bcrypt.genSalt();
+        body.password = await bcrypt.hash(body.password, salt);
+        const newUser =await User.create(body);
+        console.log('Saved user:', newUser);
         res.redirect('/login');
       }
-    } catch (error) {
-      console.error('Error during registration:', error);
-      res.status(500).json({ error: error.message });
+    } catch (err) {
+      const errors = handleErrors(err);
+      res.status(500).json(errors);
     }
   };
 
@@ -53,20 +74,21 @@ home_GET =  async(req,res)=>{
       if(token){
             const decode = jwt.decode(token);
             if(decode && decode.name){
-                res.render('home');
+              res.render('home');
             }
             else {
-                throw error;
+              throw error;
             }
       } 
       else{
-          throw error;
+        throw error;
       }  
     }
     catch (error) {
-        res.render('beforeCookie');
-      }
-  }  
+      res.render('beforeCookie');
+     }
+  };
+
 
 
   module.exports = {login_GET,login_POST,register_GET,register_POST,home_GET};
